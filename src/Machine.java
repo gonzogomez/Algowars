e7db32c31a5da9c57575eddeb42c3c1e8245add9
unknown <ggomez@.adit.mines.edu>
2013-05-01 17:07:21 -0600

    Validation code completed

--- src/Machine.java
+++ src/Machine.java
@@ -1,8 +1,16 @@
 import java.util.ArrayList;
 
 
-public class Machine {
+public class Machine implements Cloneable {
 	public int id;
 	public int speed;
 	public ArrayList<Task> tasks = new ArrayList<Task>();
+
+	public Object clone() throws CloneNotSupportedException{
+		
+			Machine cloned = (Machine)super.clone();
+			cloned.tasks = (ArrayList<Task>) tasks.clone();
+			return cloned;
+		
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,13 +11,18 @@ import java.util.Scanner;
 
 public class Main {
 	public int numTasks, numMachines;
+	public int outNumMachines;
 	public double average;
 	public ArrayList<Task> taskArray = new ArrayList<Task>();
 	public ArrayList<Machine> machineArray = new ArrayList<Machine>();
+	public ArrayList<Machine> machineSolutionArray = new ArrayList<Machine>();
+	public ArrayList<Machine> validationMachines = new ArrayList<Machine>();
+	public int indexFastest;
 	
 	
-	public void loadFile() throws FileNotFoundException {
-		Scanner scan = new Scanner(new FileReader("algoWars"));
+	
+	public void loadFile(String fileName) throws FileNotFoundException {
+		Scanner scan = new Scanner(new FileReader(fileName));
 		numTasks = scan.nextInt();
 		scan.nextLine();
 		numMachines = scan.nextInt();
@@ -36,6 +45,15 @@ public class Main {
 			sum = sum + t.speed;
 		}
 		average = sum / numTasks;
+		int max = 0;
+		int i = 0;
+		for (Machine m: machineArray){
+			if (m.speed > max){
+				max = m.speed;
+				indexFastest = i;
+			}
+			++i;
+		}
 		
 	}
 	
@@ -46,16 +64,12 @@ public class Main {
 		int index = 0;
 		
 		for (Machine m: machineArray){
-			if (m.speed > max){
-				max = m.speed;
-				index = i;
-			}
-			++i;
+			m.tasks.clear();
 		}
 		
 		Random rand = new Random();
 		for (Task t: taskArray){
-			if (t.speed > average){
+			if (c == 0 || (numMachines%c) == 0){
 				machineArray.get(index).tasks.add(t);
 			}
 			else {
@@ -82,25 +96,113 @@ public class Main {
 		return max;
 	}
 	
-	public void printAllocations() {
-		for (Machine m: machineArray) {
+	public void printAllocations(BufferedWriter bw) throws IOException {
+		
+		for (Machine m: machineSolutionArray) {
 			for (Task t: m.tasks){
-				System.out.print(t.id+1 + " ");
+				System.out.print(t.id + " ");
+				bw.write(t.id + " ");
 			}
+			
 			if (m.tasks.size() == 0) {
 				System.out.print("NONE");
+				bw.write("");
 			}
+			bw.write('\n');
 			System.out.println();
 		}
+		
+	}
+	
+	public double findSolution() throws CloneNotSupportedException {
+		double min = Double.MAX_VALUE;
+		double temp;
+		for (int i = 0; i < 20000000; ++i) {
+			allocation();
+			temp = findExecutionTime();
+			if (temp < min) {
+				min = temp;
+				machineSolutionArray.clear();
+				for (Machine m: machineArray) {
+					machineSolutionArray.add((Machine) m.clone());
+				}
+			}
+		}
+		return min;
+	}
+	
+	public boolean validation(String filename) throws FileNotFoundException {
+		Scanner scan = new Scanner(new FileReader(filename));
+		double actualValue;
+		double inputValue;
+		loadFile("group5.txt");
+		for(int i = 0; i < numMachines; i++) {
+			String line = scan.nextLine();
+			String [] tasks = line.split(" ");
+			Machine m = new Machine();
+			for (String t: tasks){
+				if (t.equals("")) {
+					m.id = machineArray.get(i).id;
+					m.speed = machineArray.get(i).speed;
+				}
+				else {
+					int index = Integer.parseInt(t);
+
+					m.tasks.add(taskArray.get(index));
+					m.id = machineArray.get(i).id;
+					m.speed = machineArray.get(i).speed;
+				}
+			}
+			validationMachines.add(m);
+		}
+		
+		inputValue = Double.parseDouble(scan.nextLine());
+		
+		actualValue = 0;
+		for (Machine m: validationMachines) {
+			int sum = 0;
+			double temp;
+			for(Task t: m.tasks) {
+				sum = sum + t.speed;
+			}
+			temp = sum / (double)m.speed;
+			if (temp > actualValue) {
+				actualValue = temp;
+			}
+		}
+		String actual = String.format("%.2f", actualValue);
+		actualValue = Double.parseDouble(actual);
+		if (actualValue == inputValue) {
+			return true;
+		}
+		
+		else {
+			return false;
+		}
+
 	}
 	
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws CloneNotSupportedException, IOException {
 		double min;
 		Main main = new Main();
-		main.loadFile();
-		main.allocation();
-		main.printAllocations();
-		System.out.println(main.findExecutionTime());
+//		main.loadFile("group5.txt");
+//		FileWriter fw = new FileWriter("output.txt");
+//		BufferedWriter bw = new BufferedWriter(fw);
+//		min = main.findSolution();
+//		main.printAllocations(bw);
+//		System.out.printf("%.2f", min);
+//		String newMin = String.format("%.2f", min);
+//		bw.write(newMin);
+//		bw.close();
+//		System.out.println()
+		boolean valid = main.validation("output.txt");
+		
+		if (valid) {
+			System.out.println("Solution is valid");
+		}
+		else {
+			System.out.println("Solution is not valid");
+		}
 	}
 }
